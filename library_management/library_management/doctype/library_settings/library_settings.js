bf7c407b1aa403c98cd922fff61c5c63244ab425
ayush jain <aj350422@gmail.com>
2024-06-11 18:22:23 +0530

    library management app

---
+++ library_management/library_management/doctype/library_settings/library_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ayush jain and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Library Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ library_management/library_management/doctype/library_settings/library_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Ayush jain and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class LibrarySettings(Document):
+	pass
